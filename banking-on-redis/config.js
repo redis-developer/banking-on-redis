017a99af297c8ea910aebbaa17df783da79e2268
Justin Castilla <pxlperfection@gmail.com>
2023-04-06 18:30:16 -0700

    adds workshop folder structure

--- banking-on-redis/config.js
+++ banking-on-redis/config.js
@@ -1,6 +1,6 @@
 export const config = {
     expressPort: process.env.SERVER_PORT ?? 8080,
-    redisHost: process.env.REDIS_HOST ?? 'localhost',
+    redisHost: process.env.REDIS_HOST ?? '127.0.0.1',
     redisPort: process.env.REDIS_PORT ?? 6379,
     redisUsername: process.env.REDIS_USERNAME || 'bob',
     redisPassword: process.env.REDIS_PASSWORD || 'foobared'
